094ae6117f85da8a283e49d596810a9e237ca4b9
beratcan97 <beratcan_acil@hotmail.se>
2020-02-24 03:20:32 +0100

    api changes to coinbase

--- src/app/components/start/start.component.ts
+++ src/app/components/start/start.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BitcoinDataService } from '../../services/bitcoin-data.service'
 import { CurrencyConverterService } from '../../services/currency-converter.service'
-import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-start',
@@ -10,8 +9,8 @@ import { stringify } from 'querystring';
 })
 export class StartComponent implements OnInit {
 
-  btcPrice;
-  btcPriceInSek;
+  btcPriceInSEK;
+  btcPriceInUSD;
 
   constructor(
     private bitcoinDataService: BitcoinDataService,
@@ -23,33 +22,20 @@ export class StartComponent implements OnInit {
     setInterval(() => {
       this.getDATA();
     }, 10000)
-
-
   }
 
   getDATA(): void {
-    this.bitcoinDataService.btcDATA$
-      .subscribe(
-        DATA => {
-          const tmp = DATA.bpi.USD.rate;
-          this.btcPrice = tmp.substring(0, tmp.length - 5);
-
-          this.currencyConverterService.exchangeDATA$
-            .subscribe(
-              DATA => {
-                const tmp = DATA['Realtime Currency Exchange Rate'];
-                const USDToSEKExchangeRate = tmp['9. Ask Price'];
-                const btcPriceWithoutDot = parseFloat(this.btcPrice.replace(',', ''));
-                const USDToSEKExchangeRateWithoutDot = parseFloat(USDToSEKExchangeRate.replace(',', ''));
-                const btcPriceInSektmp = btcPriceWithoutDot * USDToSEKExchangeRateWithoutDot;
-                this.btcPriceInSek = Math.round(btcPriceInSektmp);
-              },
-              error => console.log(error),
-            );
-        },
-        error => console.log(error),
-      );
-
-
+    this.bitcoinDataService.getBTCDataFromCoinBase('SEK')
+    .subscribe(
+      DATA => {
+        this.btcPriceInSEK = Math.round(DATA.data.amount);
+      }
+    );
+    this.bitcoinDataService.getBTCDataFromCoinBase('USD')
+    .subscribe(
+      DATA => {
+        this.btcPriceInUSD = Math.round(DATA.data.amount);
+      }
+    );
   }
 }
--- src/app/services/bitcoin-data.service.ts
+++ src/app/services/bitcoin-data.service.ts
@@ -7,11 +7,13 @@ import { HttpClient } from '@angular/common/http';
 })
 export class BitcoinDataService {
 
-  public readonly btcDATA$: Observable<any> = this.getBTCData();
-
   constructor(private http: HttpClient) { }
 
-  getBTCData(): Observable<any> {
-    return this.http.get('https://api.coindesk.com/v1/bpi/currentprice.json');
+  // getBTCData(): Observable<any> {
+  //   return this.http.get('https://api.coinbase.com/v2/prices/spot?currency=SEK');
+  // }
+
+  getBTCDataFromCoinBase(currency): Observable<any> {
+    return this.http.get('https://api.coinbase.com/v2/prices/spot?currency=' + currency);
   }
 }
